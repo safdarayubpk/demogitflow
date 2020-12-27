ffed883fd97e49d65012188424fe42390d24c98e
safdarayub <safdarayub@gmail.com>
2020-12-27 18:58:34 +0500

    add name

---
+++ name.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def show(self):
+        print(f"My name is {self.name} and age is {self.age}")
